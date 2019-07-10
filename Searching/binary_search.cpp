dbb1574736812cc8da3c34c25e2b4cf7cf12cdfd
nithintsk <52734841+nithintsk@users.noreply.github.com>
2019-07-10 16:01:36 +0530

    Recursive Binary Search

--- Searching/binary_search.cpp
+++ Searching/binary_search.cpp
@@ -3,26 +3,27 @@
 using namespace std;
 
 int* initialize(int &, int &);
-int binary_search_r (int *, int, int, int);
+int binary_search_r (int *, int, int, int, int);
 
 int main() {
 	int size;
 	int x;
 	int *arr = initialize(size,x);
-	cout<<"Index of the element is: "<<binary_search_r(arr,0,size-1,x);
+	cout<<"Index of the element is: "<<binary_search_r(arr,0,size-1,x, size);
 }
 
-int binary_search_r (int *arr, int l, int r, int x) {
+int binary_search_r (int *arr, int l, int r, int x, int size) {
+	if (l>r || r<l) {
+		return -1;
+	}
 	int mid = l + (r-l)/2;
 	if (arr[mid] == x) {
 		return mid;
 	} else if (arr[mid] > x) {
-		return binary_search_r(arr, l, mid-1, x);
-	} else if (arr[mid] < x){
-		return binary_search_r(arr, mid+1, r, x);
+		return binary_search_r(arr, l, mid-1, x, size);
 	} else {
-		return -1;
-	}
+		return binary_search_r(arr, mid+1, r, x, size);
+	} 
 }
 
 int* initialize(int &size, int &x) {
